e25279fe85a2ce53217472c1c1497de6f4b64f8c
Amin Ramezani <aminramezani2008@gamil.com>
2026-02-16 21:16:47 +0330

    the second commit

--- app/src/main/java/com/example/firstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
 
         android.widget.Toast.makeText(this, "i am in develop branch", Toast.LENGTH_SHORT).show()
+        android.widget.Toast.makeText(this, "ami ramezani in class", Toast.LENGTH_SHORT).show()
 
         kotlin.io.println("amin")
 
